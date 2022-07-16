839a30f2ef05b78a78dacd08351b84608c59776c
YehanYeshminda <81465670+YehanYeshminda@users.noreply.github.com>
2022-07-16 22:35:21 +0530

    added navbar with routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
+import './App.css';
+
+// imports of the pages
 import Navbar from './components/Navbar';
+import Homepage from './components/Homepage';
+import Exchanges from './components/Exchanges';
+import CrytoDetails from './components/CrytoDetails';
+import News from './components/News';
+import CryptoCurrencies from './components/CryptoCurrencies';
 
 function App() {
 	return (
@@ -8,7 +16,30 @@ function App() {
 			<div className="navbar">
 				<Navbar />
 			</div>
-			<div className="main"></div>
+			<div className="main">
+				<Layout>
+					<div className="routes">
+						{/* routing done with 5.2 */}
+						<Switch>
+							<Route exact path="/">
+								<Homepage />
+							</Route>
+							<Route exact path="/exchanges">
+								<Exchanges />
+							</Route>
+							<Route exact path="/crytoCurrencies">
+								<CryptoCurrencies />
+							</Route>
+							<Route exact path="/cryto/:coinid">
+								<CrytoDetails />
+							</Route>
+							<Route exact path="/news">
+								<News />
+							</Route>
+						</Switch>
+					</div>
+				</Layout>
+			</div>
 			<div className="footer"></div>
 		</div>
 	);
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as router } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as router } from 'react-router-dom';
+import 'antd/dist/antd.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-	<React.StrictMode>
+	<BrowserRouter>
 		<App />
-	</React.StrictMode>
+	</BrowserRouter>
 );
